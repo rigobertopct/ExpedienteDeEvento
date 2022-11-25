271949adfda9958c25e57395743a8572460af0d5
keniavp <you@example.com>
2022-11-25 15:36:03 -0500

    aa

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,6 +4,13 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-from apps.home.models import *
+
 # Register your models here.
+from apps.home.models import *
+
 admin.site.register(Deporte)
+admin.site.register(Disciplina)
+admin.site.register(Atleta)
+admin.site.register(CodEvento)
+
+
--- apps/home/models.py
+++ apps/home/models.py
@@ -11,8 +11,10 @@ from django.contrib.auth.models import User
 
 class Deporte(models.Model):
     nombre = models.CharField(max_length=250, verbose_name="Nombre del deporte")
+
     def __str__(self):
         return self.nombre
+
     class Meta:
         verbose_name = 'Deporte'
         verbose_name_plural = 'Deportes'
@@ -22,9 +24,64 @@ class Deporte(models.Model):
 class Disciplina(models.Model):
     nombre = models.CharField(max_length=250, verbose_name="Nombre de la disciplina")
     deporte_id = models.ForeignKey(Deporte, on_delete=models.SET_NULL, null=True, blank=True)
+
     def __str__(self):
         return self.nombre
+
     class Meta:
-        verbose_name = 'Disciplina'
-        verbose_name_plural = 'disciplina'
+        verbose_name = 'disciplina'
+        verbose_name_plural = 'disciplinas'
         db_table = 'disciplinas'
+
+
+class CodEvento(models.Model):
+    nombre = models.CharField(max_length=250, verbose_name="Nombre de la disciplina")
+    descripcion = models.CharField(max_length=1000, verbose_name="Descripción")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'codEvento'
+        verbose_name_plural = 'codEventos'
+        db_table = 'codEventos'
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=250, verbose_name="Nombre de la disciplina")
+    sigla = models.CharField(max_length=250, verbose_name="Siglas")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'pais'
+        verbose_name_plural = 'paises'
+        db_table = 'paises'
+
+
+class Atleta(models.Model):
+    nombre = models.CharField(max_length=250, verbose_name="Nombre")
+    apel = models.CharField(max_length=250, verbose_name="Apellido")
+    apel = models.CharField(max_length=250, verbose_name="Apellido")
+    fecha = models.DateField(verbose_name="Fecha de Nacimiento")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'atletas'
+        verbose_name_plural = 'atletas'
+        db_table = 'atletas'
+
+
+class Participacion(models.Model):
+    evento_id = models.ForeignKey(CodEvento, on_delete=models.SET_NULL, null=True, blank=True)
+    disciplina_id = models.ForeignKey(Disciplina, on_delete=models.SET_NULL, null=True, blank=True)
+    atleta_id = models.ForeignKey(Atleta, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = 'participacion'
+        verbose_name_plural = 'participacions'
+        db_table = 'participacions'
