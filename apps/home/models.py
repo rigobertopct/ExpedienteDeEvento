3d94047baf6a9b367e37dd7577937b95faaac074
Rigoberto <rigobertopct@gmail.com>
2022-11-24 23:44:57 -0500

    ModeloDePrueba

--- apps/home/models.py
+++ apps/home/models.py
@@ -6,5 +6,11 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 from django.contrib.auth.models import User
 
+class ModeloPrueba(models.Model):
+    nombre=models.CharField(max_length=20)
+    edad=models.IntegerField()
+    class Meta:
+        verbose_name="Prueba"
+        verbose_name_plural="Pruebas"
 # Create your models here.
 
